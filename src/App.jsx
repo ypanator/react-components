be2e2422806f55ac45d9b783ac5b24d64ccf162b
ypanator <153828092+ypanator@users.noreply.github.com>
2024-11-25 11:34:10 +0000

    Create project structure

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Accordion from "./components/accordion/Accordion";
+
+const App = () => {
+    return <Accordion />
+}
+
+export default App;
---
+++ src/components/accordion/Accordion.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Accordion.css";
+
+const Accordion = () => {
+    return (
+        <div className="accordion">
+            Hello world!
+        </div>
+    )
+}
+
+export default Accordion;
