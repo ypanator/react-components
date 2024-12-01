1b860484071b861843cc4a46cc2553ae62b4e32b
ypanator <karlossokolowski75@gmail.com>
2024-12-01 21:09:42 +0100

    Progress on accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import Accordion from "./components/accordion/Accordion";
+import "./App.css"
 
 const App = () => {
-    return <Accordion />
+    const datain = {
+        1: {
+            isOpen: false,
+            title: "title1",
+            text: "text1"
+        },
+        2: {
+            isOpen: false,
+            title: "title2",
+            text: "text2"
+        },
+        3: {
+            isOpen: false,
+            title: "title3",
+            text: "text3"
+        }
+    }
+
+    return <Accordion datain={datain} closeOthers={true}/>
 }
 
 export default App;
--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Accordion.css";
 
-const Accordion = () => {
+
+const Accordion = ({ datain, closeOthers }) => {
+
+    const [data, setData] = useState(datain);
+    
+    function handleClick(changedKey) {
+        setData((prevData) => {
+            let newData = {};
+    
+            Object.keys(prevData).forEach((key) => {
+                if (key === changedKey) {
+                    newData[key] = {
+                        ...prevData[key],
+                        isOpen: !prevData[key].isOpen
+                    };
+                } else {
+                    newData[key] = {
+                        ...prevData[key],
+                        isOpen: closeOthers ? false : prevData[key].isOpen
+                    };
+                };
+            });
+            
+            return newData;
+        });
+    }
+    
+    const elements = Object.keys(data).map((key) => {
+        const el = data[key];
+        return (
+            <div key={key} className="element">
+                <div className="element-header">
+                    <div className="header-text">
+                        {el.title}
+                    </div>
+                    <button className="header-button" onClick={() => handleClick(key)}>
+                        {el.isOpen ? "-" : "+"}
+                    </button>
+                </div>
+                <div className={`element-body ${!el.isOpen && "hidden"}`}>
+                    {el.text}
+                </div>
+            </div>
+        );
+    })
+    
     return (
-        <div className="accordion">
-            Hello world!
+        <div className="container">
+            {elements}
         </div>
-    )
+    )    
 }
 
 export default Accordion;
